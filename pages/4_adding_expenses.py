d18ee6e3252d461464b0a98c186e027022cff973
esteanes <19161310+ESteanes@users.noreply.github.com>
2025-01-15 22:01:42 +1100

    Adding in edit and delete functionality

--- pages/4_adding_expenses.py
+++ pages/4_adding_expenses.py
@@ -18,26 +18,6 @@ def refresh_all_the_data():
     utils.fetch_spending_data.clear()
 
 
-def clean_transaction_data(transaction_data: pd.DataFrame):
-    clean = transaction_data.rename(columns={
-        "Category": "Upbank Category",
-        "empty": "Quantity",
-        "Empty": "Measure",
-        "rawText": "Upbank Text",
-        "description": "Shop",
-        "empty_1": "Details",
-        "empty_2": "Tag",
-        "createdAt": "Date"
-    })
-    clean['Cost'] = clean['Cost'] * -1
-    clean['Item'] = pd.Series(dtype='str')
-    clean['Location'] = pd.Series(dtype='str')
-    clean['Date'] = pd.to_datetime(clean['Date'])
-    clean['Details'] = clean['Details'].dropna().astype(str)
-    clean['Tag'] = clean['Tag'].dropna().astype(str)
-    return clean
-
-
 def get_info(spending_data: pd.DataFrame):
     column_config = {
         "Item": st.column_config.SelectboxColumn(
@@ -119,7 +99,170 @@ def save_edited_values(edited_df: pd.DataFrame):
     refresh_all_the_data()
 
 
-def initialise_sidebar(inputs: DeltaGenerator):
+@st.dialog("Add expenses")
+def add_expenses(categorised_transactions: pd.DataFrame):
+    sorted_df = categorised_transactions.sort_values(by=['Date'], ascending=False)
+    with st.expander("Duplicate an existing expense entry"):
+        prior_expenses_entry = st.dataframe(
+            sorted_df,
+            use_container_width=True,
+            hide_index=True,
+            on_select="rerun",
+            selection_mode="single-row"
+        )
+    # Form fields
+    if prior_expenses_entry.selection.rows:
+        new_row = handle_selection_and_prefill(prior_expenses_entry, sorted_df)
+    else:
+        new_row = handle_selection_and_prefill(None, sorted_df)
+    if st.button("Submit"):
+        edited_df = pd.concat(
+            [categorised_transactions, pd.DataFrame([new_row])],
+            ignore_index=True
+        )[utils.SPENDING_DATA_SCHEMA]
+        utils.save_data(
+            edited_df,
+            utils.SPENDING_PATH,
+            utils.SPENDING_SHEET_NAME)
+        utils.fetch_income_deduction_data.clear()
+        st.rerun()
+
+
+@st.dialog("Delete expenses")
+def delete_expenses(categorised_transactions: pd.DataFrame):
+    sorted_df = categorised_transactions.sort_values(by=['Date'], ascending=False)
+    prior_expenses_entry = st.dataframe(
+        sorted_df,
+        use_container_width=True,
+        hide_index=True,
+        on_select="rerun",
+        selection_mode="single-row"
+    )
+    if prior_expenses_entry.selection.rows:
+        st.write(prior_expenses_entry.selection.rows[0])
+        st.write(sorted_df.iloc[prior_expenses_entry.selection.rows[0]])
+        index_label = sorted_df.index[prior_expenses_entry.selection.rows[0]]
+        edited_df = (
+            sorted_df
+            .drop(index=index_label)
+            [utils.SPENDING_DATA_SCHEMA]
+            .sort_values(by=["Date"])
+            .reset_index()
+        )
+        st.dataframe(edited_df)
+        if st.button("Delete Transaction"):
+            utils.save_data(
+                edited_df,
+                utils.SPENDING_PATH,
+                utils.SPENDING_SHEET_NAME)
+            utils.fetch_income_deduction_data.clear()
+            st.rerun()
+
+
+def unique_items_and_index(series: pd.Series, value):
+    unique_series = series.drop_duplicates().reset_index(drop=True)
+    if value:
+        index = int(unique_series[unique_series == value].index[0])
+    else:
+        index = 0
+    return unique_series, index
+
+
+@st.dialog("Edit expenses")
+def edit_expenses(categorised_transactions: pd.DataFrame):
+    sorted_df = categorised_transactions.sort_values(by=['Date'], ascending=False)
+    prior_expenses_entry = st.dataframe(
+        sorted_df,
+        use_container_width=True,
+        hide_index=True,
+        on_select="rerun",
+        selection_mode="single-row"
+    )
+
+    if prior_expenses_entry.selection.rows:
+        # Get the data for the selected row
+        new_row = handle_selection_and_prefill(prior_expenses_entry, sorted_df)
+        if st.button("Edit transaction"):
+            sorted_df.iloc[prior_expenses_entry.selection.rows[0]] = new_row
+            edited_df = sorted_df[utils.SPENDING_DATA_SCHEMA].sort_values(by=["Date"]).reset_index()
+            utils.save_data(
+                edited_df,
+                utils.SPENDING_PATH,
+                utils.SPENDING_SHEET_NAME)
+            utils.fetch_income_deduction_data.clear()
+            st.rerun()
+
+
+def handle_selection_and_prefill(prior_expenses_entry, sorted_df: pd.DataFrame):
+    item_value = None
+    cost_value = None
+    quantity_value = None
+    measure_value = None
+    location_value = None
+    shop_value = None
+    details_value = None
+    tag_value = None
+    date_value = None
+    receipt_ref_value = None
+    receipt_value = None
+    transaction_id_value = None
+    if prior_expenses_entry:
+        selected_row_data = sorted_df.iloc[prior_expenses_entry.selection.rows[0]]
+        item_value = selected_row_data["Item"]
+        cost_value = selected_row_data["Cost"]
+        quantity_value = selected_row_data["Quantity"]
+        measure_value = selected_row_data["Measure"]
+        location_value = selected_row_data["Location"]
+        shop_value = selected_row_data["Shop"]
+        details_value = selected_row_data["Details"]
+        tag_value = selected_row_data["Tag"]
+        date_value = selected_row_data["Date"]
+        receipt_ref_value = selected_row_data["Receipt Ref"]
+        receipt_value = selected_row_data["Receipt"]
+        transaction_id_value = selected_row_data["transactionId"]
+    item_unique, item_index = unique_items_and_index(sorted_df.Item, item_value)
+    item = st.selectbox("Item", options=np.append(
+        item_unique,
+        st.session_state.additional_items), index=item_index)
+    cost = st.number_input("Cost", min_value=-90000.0, format="%.2f", value=cost_value)
+    quantity = st.number_input("Quantity", min_value=0.0, format="%.2f", value=quantity_value)
+    measure_unique, measure_index = unique_items_and_index(sorted_df.Measure, measure_value)
+    measure = st.selectbox("Measure", options=np.append(
+        measure_unique,
+        st.session_state.additional_items), index=measure_index)
+    location_unique, location_index = unique_items_and_index(sorted_df.Location, location_value)
+    location = st.selectbox("Location", options=np.append(
+        location_unique,
+        st.session_state.additional_items), index=location_index)
+    shop_unique, shop_index = unique_items_and_index(sorted_df.Shop, shop_value)
+    shop = st.selectbox("Shop", options=np.append(
+        shop_unique,
+        st.session_state.additional_items), index=shop_index)
+    details = st.text_area("Details", value=details_value)
+    tag_unique, tag_index = unique_items_and_index(sorted_df.Tag, tag_value)
+    tag = st.selectbox("Location", options=np.append(
+        tag_unique,
+        st.session_state.additional_items), index=tag_index)
+    date = st.date_input("Date", value=date_value, max_value=pd.Timestamp.today() + pd.DateOffset(months=2))
+    # Create a dictionary from the modal data
+    new_row = {
+        "Item": item,
+        "Cost": cost,
+        "Quantity": quantity,
+        "Measure": measure,
+        "Location": location,
+        "Shop": shop,
+        "Details": details,
+        "Tag": tag,
+        "Date": pd.Timestamp(date),
+        "Receipt Ref": "",
+        "Receipt": "",
+        "transactionId": transaction_id_value
+    }
+    return new_row
+
+
+def initialise_sidebar(inputs: DeltaGenerator, categorised_transactions: pd.DataFrame):
     inputs.sidebar.button("Refresh Data", on_click=refresh_all_the_data)
     if "additional_items" not in st.session_state:
         st.session_state.additional_items = []
@@ -139,126 +282,27 @@ def initialise_sidebar(inputs: DeltaGenerator):
         inputs.sidebar.write("Addtional item names:")
         inputs.sidebar.write(inputs.session_state.additional_items)
     inputs.title("Categorise Expenses from Upbank or Add Expenses")
+    col1, col2, col3 = st.sidebar.columns(3)
+    if col1.button("Add"):
+        add_expenses(categorised_transactions)
+    if col2.button("Edit"):
+        edit_expenses(categorised_transactions)
+    if col3.button("Remove"):
+        delete_expenses(categorised_transactions)
     return start_date, end_date
 
 
-@st.dialog("Add expenses")
-def add_expenses(existing_expenses: pd.DataFrame):
-    with st.expander("Duplicate an existing expense entry"):
-        prior_expenses_entry = st.dataframe(
-            existing_expenses,
-            use_container_width=True,
-            hide_index=True,
-            on_select="rerun",
-            selection_mode="single-row"
-        )
-    # Form fields
-    if prior_expenses_entry.selection.rows:
-        # Get the data for the selected row
-        selected_row_data = existing_expenses.iloc[prior_expenses_entry.selection.rows[0]]
-        item_value = selected_row_data["Item"]
-        cost_value = selected_row_data["Cost"]
-        quantity_value = selected_row_data["Quantity"]
-        measure_value = selected_row_data["Measure"]
-        location_value = selected_row_data["Location"]
-        shop_value = selected_row_data["Shop"]
-        details_value = selected_row_data["Details"]
-        tag_value = selected_row_data["Tag"]
-        date_value = selected_row_data["Date"]
-        receipt_ref_value = selected_row_data["Receipt Ref"]
-        receipt_value = selected_row_data["Receipt"]
-        transaction_id_value = selected_row_data["transactionId"]
-    else:
-        # Default values if no row is selected
-        item_value = ""
-        cost_value = 0.0
-        quantity_value = 0.0
-        measure_value = ""
-        location_value = ""
-        shop_value = ""
-        details_value = ""
-        tag_value = ""
-        date_value = pd.Timestamp.now()
-        receipt_ref_value = ""
-        receipt_value = ""
-        transaction_id_value = ""
-
-    # Form fields
-    item = st.selectbox("Item", options=np.append(
-        existing_expenses.Item.unique(),
-        st.session_state.additional_items), placeholder=item_value)
-    cost = st.number_input("Cost", min_value=0.0, format="%.2f", value=cost_value)
-    quantity = st.number_input("Quantity", min_value=0.0, format="%.2f", value=quantity_value)
-    measure = st.selectbox("Measure", options=np.append(
-        existing_expenses.Measure.unique(),
-        st.session_state.additional_items), placeholder=measure_value)
-    location = st.selectbox("Location", options=np.append(
-        existing_expenses.Location.unique(),
-        st.session_state.additional_items), placeholder=location_value)
-    shop = st.selectbox("Shop", options=np.append(
-        existing_expenses.Shop.unique(),
-        st.session_state.additional_items), placeholder=shop_value)
-    details = st.text_area("Details", value=details_value)
-    tag = st.selectbox("Location", options=np.append(
-        existing_expenses.Tag.unique(),
-        st.session_state.additional_items), placeholder=tag_value)
-    date = st.date_input("Date", value=date_value, max_value=pd.Timestamp.today())
-
-    ### Currently don't support adding receipts yet
-
-    # receipt_ref = st.selectbox("Receipt Ref", options=np.append(
-    #     existing_expenses['Receipt Ref'].unique(),
-    #     st.session_state.additional_items), placeholder=receipt_ref_value)
-    # receipt = st.selectbox("Receipt", options=np.append(
-    #     existing_expenses['Receipt'].unique(),
-    #     st.session_state.additional_items), placeholder=receipt_value)
-
-    transaction_id = st.text_input("Employer", value=transaction_id_value)
-
-    if st.button("Submit"):
-        # Create a dictionary from the modal data
-        new_row = {
-            "Item": item,
-            "Cost": cost,
-            "Quantity": quantity,
-            "Measure": measure,
-            "Location": location,
-            "Shop": shop,
-            "Details": details,
-            "Tag": tag,
-            "Date": date,
-            "Receipt Ref": "",
-            "Receipt": "",
-            "transactionId": transaction_id
-        }
-        edited_df = pd.concat(
-            [existing_expenses, pd.DataFrame([new_row])],
-            ignore_index=True
-        )[utils.INCOME_DATA_SCHEMA]
-        utils.save_data(
-            edited_df,
-            utils.SPENDING_PATH,
-            utils.SPENDING_SHEET_NAME)
-        utils.fetch_income_deduction_data.clear()
-        st.rerun()
-
-
 def render_transaction_input(inputs: DeltaGenerator):
-    start_date, end_date = initialise_sidebar(inputs)
+    categorised_transactions = utils.fetch_spending_data()
+    start_date, end_date = initialise_sidebar(inputs, categorised_transactions)
     transactions_data = utils.fetch_transaction_data(start_date, end_date)
     if transactions_data.empty:
         st.write("No transaction data fetched")
         return
-
-    categorised_transactions = utils.fetch_spending_data()
-    col1, col2 = st.sidebar.columns(2)
-    if col1.button("Add Income Data"):
-        add_expenses(categorised_transactions)
     # only show the uncategorised transactions
     uncategorised_transactions = transactions_data[
         ~transactions_data['transactionId'].isin(categorised_transactions['transactionId'])
     ]
-    uncategorised_transactions = clean_transaction_data(uncategorised_transactions)
 
     inputs.subheader("Uncategorised expenses")
     column_config, column_order = get_info(categorised_transactions)
