f175891142f8192609ed0084741e73e8c3c5ccce
esteanes <19161310+ESteanes@users.noreply.github.com>
2025-03-17 23:23:29 +1100

    Improving the main page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 import classes.spending as spending
 import utils
 
@@ -8,13 +8,32 @@ st.set_page_config(
     page_icon=":money:"
 )
 
-st.write("""
-This is the landing page
-""")
 
-st.write(utils.INCOME_PATH)
-st.write(spending.SPENDING_PATH)
+income, deductions = utils.fetch_income_deduction_data()
+spending_data = spending.SpendingData().fetch_spending_data()
 
-utils.fetch_income_deduction_data()
-spending.SpendingData().fetch_spending_data()
-utils.fetch_transaction_data()
+transaction_data = utils.fetch_transaction_data()
+uncategorised_transactions = transaction_data[
+        ~transaction_data['transactionId'].isin(spending_data.spending['transactionId'])
+    ]
+col1, col2, col3 = st.columns(3)
+col1.metric("No. uncategorised transactions", len(uncategorised_transactions))
+col1.metric("No. transactions from Up Bank", len(transaction_data))
+
+col2.metric("Latest Transaction", spending_data.spending['Date'].max().strftime('%Y-%m-%d'), delta=-int(pd.Timedelta(pd.Timestamp.now() - spending_data.spending['Date'].max()).total_seconds()/(60*60*24)))
+col2.metric("Number of transactions recorded:", len(spending_data.spending))
+
+col3.metric("Number of incomes recorded", len(income))
+col3.metric("Most recent income recorded", income['Date'].max().strftime("%Y-%m-%d"), delta=-int(pd.Timedelta(pd.Timestamp.now() - income['Date'].max()).total_seconds()/(60*60*24)))
+
+st.divider()
+a, b, c, d = st.columns(4)
+a.metric("Rows in location table", len(spending_data.location))
+b.metric("Rows in top table", len(spending_data.top_table))
+c.metric("Rows in middle table", len(spending_data.middle_table))
+d.metric("Rows in base table", len(spending_data.base_table))
+
+st.divider()
+
+st.write(f"Income path: `{utils.INCOME_PATH}`")
+st.write(f"Spending path: `{spending.SPENDING_PATH}`")
